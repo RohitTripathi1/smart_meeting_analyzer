bfc2e85d154a2eccfae049f8c99fdd0f3846e5bb
Rohit Tripathi <rt264@snu.edu.in>
2026-03-17 00:15:35 +0530

    initial commit project setup

---
+++ smart_meeting_analyzer/backend/src/main/java/com/example/smartmeetinganalyzer/SmartMeetingAnalyzerApplication.java
@@ -0,0 +1,13 @@
+package com.example.smartmeetinganalyzer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmartMeetingAnalyzerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SmartMeetingAnalyzerApplication.class, args);
+	}
+
+}
---
+++ smart_meeting_analyzer/backend/src/test/java/com/example/smartmeetinganalyzer/SmartMeetingAnalyzerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.smartmeetinganalyzer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SmartMeetingAnalyzerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
